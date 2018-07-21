17a343fa744e6037c6dbb3099a0dd57d54f43b2d
Micah Brown <micahyb@gmail.com>
2018-07-20 21:30:15 -0600

    added dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/dashboard" => "dashboard#index"
 end
