fb86d41464faeb993eac0bffe8ab640ebe8beef7
Micah Brown <micahyb@gmail.com>
2018-07-22 20:34:47 -0600

    added root action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/dashboard" => "dashboard#index"
+
+  root "home#index"
 end
