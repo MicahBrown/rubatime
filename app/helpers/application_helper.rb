4479ca8ab517606396565ab1f481ad6847bfc3c5
Micah Brown <micahyb@gmail.com>
2018-07-22 22:57:16 -0600

    added font awesome icon helper
    switched to webfonts (svg & js didn't work with turbolinks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     return unless datetime.present?
     datetime.in_time_zone(TIMEZONE).strftime("%b %e, %Y %-l:%M %p")
   end
+
+  def icon(icon_name, text=nil, options={})
+    options, text = text, nil if text.is_a?(Hash)
+    options.merge!(class: "fas fa-#{icon_name} #{options[:class]}")
+
+    content = content_tag(:i, nil, options)
+    content = content + " " + text if text.present?
+    content
+  end
 end
