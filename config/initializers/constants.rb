0e2a1eff673bd6b17020e2274acbb8cdafca8f76
Micah Brown <micahyb@gmail.com>
2018-07-26 18:31:23 -0600

    added list of default projects

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,17 @@
 TIMEZONE = "Mountain Time (US & Canada)".freeze
+DEFAULT_PROJECTS = [
+  "AIS Educators Website",
+  "Calendar System",
+  "EMPG 18",
+  "Farmworker",
+  "Governor's Council on Fitness",
+  "Homeland Security 17",
+  "Immunize Kansas Coalition",
+  "IT - System Admin",
+  "Maternal Child Health Council",
+  "Missouri Training and Exercise System",
+  "Patient Satisfaction",
+  "Prenatal Services Directory",
+  "Shepherd's Crossing - CAMS Revisions",
+  "Timelog"
+].freeze
