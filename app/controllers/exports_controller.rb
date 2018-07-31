fad6b6f2ba9b3209345bbe27e644ba8a6a80485f
Micah Brown <micahyb@gmail.com>
2018-07-31 16:17:20 -0600

    fix log query

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,7 +3,7 @@ require 'csv'
 class ExportsController < ApplicationController
   def create
     # prev_period = Log.current__pay_period_dates
-    last_month = Date.today
+    last_month = Date.today.in_time_zone(TIMEZONE)
     prev_period = last_month.beginning_of_month..last_month.end_of_month
     logs = Log.in_datetime_range(prev_period.min, prev_period.max).active.order("start_at ASC, end_at ASC")
 
@@ -17,8 +17,8 @@ class ExportsController < ApplicationController
   private
 
     def render_csv(logs, prev_period)
-      max_time = prev_period.max.in_time_zone(TIMEZONE)
-      min_time = prev_period.min.in_time_zone(TIMEZONE)
+      max_time = prev_period.max
+      min_time = prev_period.min
 
       CSV.generate(headers: false) do |csv|
         logs.includes(:project).each do |log|
@@ -27,7 +27,7 @@ class ExportsController < ApplicationController
           end_time = log.end_at.in_time_zone(TIMEZONE)
           end_time = max_time if end_time > max_time
 
-          csv << [log.id, "Micah", log.project.name, "", "", 100, "", "", start_time.to_datetime.strftime("%-m/%d/%Y %-l:%M %P"), start_time.to_date, end_time.to_datetime.strftime("%-m/%d/%Y %-l:%m %P"), hours(start_time, end_time), "FALSE", log.description]
+          csv << [log.id, "Micah", log.project.name, "", "", 100, "", "", start_time.to_datetime.strftime("%-m/%d/%Y %-l:%M %P"), start_time.to_date, end_time.to_datetime.strftime("%-m/%d/%Y %-l:%M %P"), hours(start_time, end_time), "FALSE", log.description]
         end
       end.encode('WINDOWS-1252', undef: :replace, replace: '')
     end
