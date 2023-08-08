b66a7ad3ba5a5eb2bdf86c1427d1e6955a2c0602
Micah Brown <micahyb@gmail.com>
2023-08-08 15:43:29 -0500

    show logs without end date within dashboard daterange

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,6 @@ class DashboardController < ApplicationController
     @start_date = params[:filtered_start_date]&.to_date || (2.months.ago.beginning_of_month + 1.day)
     @end_date = params[:filtered_end_date]&.to_date&.end_of_day || Date.today.end_of_month
 
-    @logs = Log.order("start_at DESC").in_datetime_range(@start_date, @end_date).page(params[:page]).per(50)
+    @logs = Log.order("start_at DESC").in_datetime_range_or_pending(@start_date, @end_date).page(params[:page]).per(50)
   end
 end
