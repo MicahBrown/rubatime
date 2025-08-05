cd715ad04d53bf48c3b16a061141a3803a681cb4
Micah Brown <micahyb@gmail.com>
2025-08-05 16:36:58 -0500

    added dashboard for daily hours

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,7 @@ class DashboardController < ApplicationController
   def index
     @start_date = params[:filtered_start_date]&.to_date || (Date.today.beginning_of_month)
     @end_date = params[:filtered_end_date]&.to_date&.end_of_day || Date.today.end_of_month
-
-    @all_logs = Log.where(active: true).in_datetime_range(@start_date.in_time_zone(TIMEZONE), @end_date.in_time_zone(TIMEZONE))
+    @all_logs = Log.where(active: true).in_datetime_range(@start_date.in_time_zone(TIMEZONE).beginning_of_day, @end_date.in_time_zone(TIMEZONE).end_of_day)
     @all_logs = @all_logs.where(project_id: params[:filtered_project]) if params[:filtered_project].present?
     @logs = @all_logs.page(params[:page]).ordered.per(50)
   end
