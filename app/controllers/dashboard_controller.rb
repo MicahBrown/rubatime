ab5131f57cfcfd24eb1dd35f2b91188dbeedb0ca
Micah Brown <micahyb@gmail.com>
2025-08-04 22:34:00 -0500

    show elapsed time for all logs (including paginated ones)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,8 @@ class DashboardController < ApplicationController
     @start_date = params[:filtered_start_date]&.to_date || (2.months.ago.beginning_of_month + 1.day)
     @end_date = params[:filtered_end_date]&.to_date&.end_of_day || Date.today.end_of_month
 
-    @logs = Log.where(active: true).in_datetime_range(@start_date, @end_date)
-    @logs = @logs.where(project_id: params[:filtered_project]) if params[:filtered_project].present?
-    @logs = @logs.page(params[:page]).ordered.per(50)
+    @all_logs = Log.where(active: true).in_datetime_range(@start_date, @end_date)
+    @all_logs = @all_logs.where(project_id: params[:filtered_project]) if params[:filtered_project].present?
+    @logs = @all_logs.page(params[:page]).ordered.per(50)
   end
 end
