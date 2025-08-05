b993ccc5984b6e22ff030b10b5ddeec3a39d267e
Micah Brown <micahyb@gmail.com>
2025-08-04 22:36:21 -0500

    default to current month

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :load_and_save_filters
 
   def index
-    @start_date = params[:filtered_start_date]&.to_date || (2.months.ago.beginning_of_month + 1.day)
+    @start_date = params[:filtered_start_date]&.to_date || (Date.today.beginning_of_month)
     @end_date = params[:filtered_end_date]&.to_date&.end_of_day || Date.today.end_of_month
 
     @all_logs = Log.where(active: true).in_datetime_range(@start_date, @end_date)
