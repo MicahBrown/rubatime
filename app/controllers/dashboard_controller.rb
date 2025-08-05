529614f4c83880bacfac5e187d2a6e0aa5b70758
Micah Brown <micahyb@gmail.com>
2025-08-05 16:25:16 -0500

    apply timezones to log daterange filter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     @start_date = params[:filtered_start_date]&.to_date || (Date.today.beginning_of_month)
     @end_date = params[:filtered_end_date]&.to_date&.end_of_day || Date.today.end_of_month
 
-    @all_logs = Log.where(active: true).in_datetime_range(@start_date, @end_date)
+    @all_logs = Log.where(active: true).in_datetime_range(@start_date.in_time_zone(TIMEZONE), @end_date.in_time_zone(TIMEZONE))
     @all_logs = @all_logs.where(project_id: params[:filtered_project]) if params[:filtered_project].present?
     @logs = @all_logs.page(params[:page]).ordered.per(50)
   end
