9278da9452e0ad4e1e72c84fdb9f16c067193641
Micah Brown <micahyb@gmail.com>
2025-08-06 17:00:09 -0500

    adjust time for timezone offset

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,6 @@
 class LogsController < ApplicationController
   def create
-    @log = Log.new(start_at: Time.now)
+    @log = Log.new(start_at: Time.now + 1.hour)
 
     if @log.save
       redirect_to edit_log_path(@log), notice: "Successfully started new log"
